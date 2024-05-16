2cac54eacac540527bdf063628e8c98aeccf0222
UsmanHashimov <hashimovusman01@gmail.com>
2024-05-16 11:19:24 +0500

    some changes

--- ResumeViewer/src/Resume/Resume.Application/UseCases/Resume/Handlers/QueryHandlers/GetAllResumeByCategoryQueryHandler.cs
+++ ResumeViewer/src/Resume/Resume.Application/UseCases/Resume/Handlers/QueryHandlers/GetAllResumeByCategoryQueryHandler.cs
@@ -1,3 +1,6 @@
+using MediatR;
+using Resume.Application.UseCases.Resume.Queries;
+using Resume.Domain.Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace Resume.Application.UseCases.Resume.Handlers.QueryHandlers
 {
-    public class GetAllResumeByCategoryQueryHandler
+    public class GetAllResumeByCategoryQueryHandler : IRequestHandler<GetAllResumeByCategoryQuery, List<ResumeModel>>
     {
+        public Task<List<ResumeModel>> Handle(GetAllResumeByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
